1bbcee5c753e0831b0ac299de80062c8bb234b63
Bill Saunders <bill.saunders@protonmail.com>
2017-09-17 15:30:38 +0100

    updates and new HTML files

--- rpg/item.py
+++ rpg/item.py
@@ -2,20 +2,26 @@
 
 class Item():
     def __init__(self, item_name, item_description):
+        """ constructs an Item and sets its name and description attributes"""
         self.name = item_name
         self.description = item_description
         
     def set_description(self, item_description):
+        """ updates the description of the Item"""
         self.description = item_description
 
     def get_description(self):
+        """ returns the description of the Item"""
         return self.description 
         
     def set_name(self, item_name):
+        """ updates the name of the Item"""
         self.name = item_name
 
     def get_name(self):
+        """ returns the name of the Item"""
         return self.name
         
     def describe(self):
+        """ prints the item object's description """
         print(self.description)
